6136ad43e5fdabd81aef7d3818af5061d9ad3720
Nitesh Mishra <nitesh.mishra3@gmail.com>
2024-01-04 18:29:12 -0800

    generated the initial script to make API call using `keyring`

---
+++ R/call_api.R
@@ -0,0 +1,51 @@
+# Install and load the httr package if you haven't already
+# install.packages("httr")
+library(httr)
+library(jsonlite)
+library(tidyverse)
+library(keyring)
+
+key <- Sys.getenv("data_gov_in_APIKEY")
+format <- "json"
+offset <- 100
+limit <- 62
+base_url <- "https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69"
+
+# create the url based on set parameters
+url <-
+  paste0(base_url, "?api-key=",
+         key, "&format=",
+         format, "&offset=",
+         offset, "&limit=", limit)
+url
+
+response <- GET(url)
+
+# Check if the request was successful (status code 200)
+if (http_status(response)$category == "Success") {
+  # Parse the JSON response
+  data <- content(response, as = "parsed", encoding = "UTF-8")
+
+  # Print or process the data as needed
+  print(data)
+} else {
+  # Print an error message if the request was not successful
+  print(paste("Error:", http_status(response)$reason))
+}
+
+json_list <-
+  toJSON(data, pretty = TRUE) |>
+  # parse to a tibble
+  jsonlite::fromJSON()
+
+data <-
+  json_list$records |>
+  tibble::as_tibble() |>
+  tidyr::unnest()
+
+# generate a filename based on current datetime
+filename <-
+  paste0("data_gov_realtime_aqi_api_", format(Sys.time(), "%Y%m%d_%H%M%S"), ".csv")
+
+# write the data to a csv file
+write_csv(data, filename)

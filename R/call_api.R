ed5d0e0931a3f5640aade852b99b3a365db5d093
Nitesh Mishra <nitesh.mishra3@gmail.com>
2024-01-11 13:48:24 -0800

    Generated docs for call_api script

--- R/call_api.R
+++ R/call_api.R
@@ -1,18 +1,26 @@
-# Install and load the httr package if you haven't already
-# install.packages("httr")
+# This script makes an API call to retrieve real-time air quality index (AQI) data from the data.gov.in website.
+# It uses the httr package to send HTTP requests and retrieve the response.
+# The retrieved data is then processed and saved to a CSV file.
+
+# Load the required packages
 library(httr)
 library(jsonlite)
 library(tidyverse)
 
+# Get the API key from the system environment variable
 key <- Sys.getenv("DATA_GOV_API_KEY")
+
+# Set the format and limit for the API request
 format <- "json"
 limit <- "all"
+
+# Set the base URL for the API
 base_url <- "https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69"
 
-# create the URL based on set parameters
+# Create the URL with the set parameters
 url <- paste0(base_url, "?api-key=", key, "&format=", format, "&limit=", limit)
 
-# Make the request and handle any errors
+# Make the API request and handle any errors
 tryCatch(
   {
     response <- GET(url)
@@ -23,7 +31,7 @@ tryCatch(
       data <- content(response, as = "parsed", encoding = "UTF-8")
 
       # Print or process the data as needed
-      cat("API call successful.\nRecieved", length(data$records), "records.\n")
+      cat("API call successful.\nReceived", length(data$records), "records.\n")
     } else {
       # Print an error message if the request was not successful
       print(paste("Error:", http_status(response)$reason))
@@ -35,11 +43,13 @@ tryCatch(
   }
 )
 
+# Process and save the data if it exists
 if (exists("data")) {
+  # Convert the JSON response to a tibble
   json_list <- toJSON(data, pretty = TRUE) |>
-    # parse to a tibble
     jsonlite::fromJSON()
 
+  # Convert the tibble to a data frame and unnest the nested columns
   data <- json_list$records |>
     tibble::as_tibble() |>
     tidyr::unnest(
@@ -49,7 +59,7 @@ if (exists("data")) {
       )
     )
 
-  # generate a filename based on the current datetime
+  # Generate a filename based on the current datetime
   filename <- paste0(
     "data/data_gov_realtime_aqi_api_",
     format(
@@ -59,6 +69,6 @@ if (exists("data")) {
     ".csv"
   )
 
-  # write the data to a CSV file
+  # Write the data to a CSV file
   write_csv(data, filename)
 }
